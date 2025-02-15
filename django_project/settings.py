4baf5f33e64ad1ab85d0c533d4ac76a0ab419fe5
Wilmer <wilmeraguirre2002.3@gmail.com>
2025-02-15 09:03:34 -0400

    pagina home

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    'pages',
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # path to templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
